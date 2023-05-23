06ef7839d0d493e1855b3538b532afc23600964d
pranay292002 <76903075+pranay292002@users.noreply.github.com>
2023-05-23 14:23:09 +0530

    Create Assignment7.py

---
+++ Assignment7.py
@@ -0,0 +1,33 @@
+# import required module
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# assign documents
+d0 = 'Just Code It'
+d1 = 'Code'
+d2 = 'Programming'
+
+# merge documents into a single corpus
+string = [d0, d1, d2]
+
+# create object
+tfidf = TfidfVectorizer()
+
+# get tf-idf values
+result = tfidf.fit_transform(string)
+
+# get idf values
+print('\nidf values:')
+for ele1, ele2 in zip(tfidf.get_feature_names(), tfidf.idf_):
+    print(ele1, ':', ele2)
+
+# get indexing
+print('\nWord indexes:')
+print(tfidf.vocabulary_)
+
+# display tf-idf values
+print('\ntf-idf value:')
+print(result)
+
+# in matrix form
+print('\ntf-idf values in matrix form:')
+print(result.toarray())
